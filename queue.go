c8b8c15dcc263aa9c166f55c066af80406a2567c
willsun <willsun@lilith.com>
2023-05-17 15:47:21 +0800

    完善代码，添加测试用例

--- queue.go
+++ queue.go
@@ -6,9 +6,8 @@ import (
 	"github.com/redis/go-redis/v9"
 )
 
-// 将一条消息msg放入到延时队列中，该消息的延时时间为delayedMs
-func Enqueue(ctx context.Context, client *redis.Client, queueID string, msg []byte, delayedMs int64) error {
-	const script = `
+const (
+	enqueueScript = `
 local key = KEYS[1]
 local msg = ARGV[1]
 local delayMs = tonumber(ARGV[2])
@@ -17,18 +16,13 @@ local tsInMs = curTimestamp[1] * 1000 + math.floor(curTimestamp[2] / 1000)
 local dueTs = tsInMs + delayMs
 return redis.call('ZADD', key, dueTs, msg)
 	`
-	c := client.Eval(ctx, script, []string{queueID}, msg, delayedMs)
-	return c.Err()
-}
 
-// 拉取已经到期的消息
-func Dequeue(ctx context.Context, client *redis.Client, queueID string, limit int64) ([][]byte, error) {
-	var msgs [][]byte
-	const script = `
+	dequeueScript = `
 local key = KEYS[1]
-local limit = ARGV[1]
+local limit = tonumber(ARGV[1])
 local curTimestamp = redis.call('TIME')
 local tsInMs = curTimestamp[1] * 1000 + math.floor(curTimestamp[2] / 1000)
+local msgs = {}
 if limit > 0 then
 	msgs = redis.call('ZRANGE', key, '-inf', tsInMs, 'BYSCORE', 'LIMIT', 0, limit)
 else
@@ -38,6 +32,64 @@ if #msgs > 0 then
 	redis.call('ZREM', key, unpack(msgs))
 end
 return msgs
-`
+	`
+)
+
+var (
+	enqueueScriptSha1 string
+	dequeueScriptSha1 string
+	isLoaded          bool
+)
+
+// 加载脚本到redis缓存
+func loadScript(ctx context.Context, client *redis.Client) error {
+	sc1 := client.ScriptLoad(ctx, enqueueScript)
+	if sc1.Err() != nil {
+		return sc1.Err()
+	}
+	sc2 := client.ScriptLoad(ctx, dequeueScript)
+	if sc2.Err() != nil {
+		return sc2.Err()
+	}
+
+	enqueueScriptSha1, _ = sc1.Result()
+	dequeueScriptSha1, _ = sc2.Result()
+
+	isLoaded = true
+	return nil
+}
+
+// 将一条消息msg放入到延时队列中，该消息的延时时间为delayedMs
+func Enqueue(ctx context.Context, client *redis.Client, queueID string, msg string, delayedMs int64) error {
+	if !isLoaded {
+		err := loadScript(ctx, client)
+		if err != nil {
+			return err
+		}
+	}
+	c := client.EvalSha(ctx, enqueueScriptSha1, []string{queueID}, msg, delayedMs)
+	return c.Err()
+}
+
+// 拉取已经到期的消息
+func Dequeue(ctx context.Context, client *redis.Client, queueID string, limit int64) ([]string, error) {
+	if !isLoaded {
+		err := loadScript(ctx, client)
+		if err != nil {
+			return nil, err
+		}
+	}
+	var msgs []string = make([]string, 0, limit)
+	cmdResult := client.EvalSha(ctx, dequeueScriptSha1, []string{queueID}, limit)
+	if cmdResult.Err() != nil {
+		return nil, cmdResult.Err()
+	}
+	rets, err := cmdResult.Slice()
+	if err != nil {
+		return nil, err
+	}
+	for i := range rets {
+		msgs = append(msgs, rets[i].(string))
+	}
 	return msgs, nil
 }
---
+++ queue_test.go
@@ -0,0 +1,74 @@
+package delayed_queue
+
+import (
+	"context"
+	"fmt"
+	"math/rand"
+	"strings"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestQ(t *testing.T) {
+	const queueID = "q_nums"
+	const connUrl = "redis://localhost:16379/0"
+	opts, err := redis.ParseURL(strings.TrimSpace(connUrl))
+	assert.Empty(t, err)
+	client := redis.NewClient(opts)
+	ctx, cf := context.WithTimeout(context.Background(), time.Second)
+	sc := client.Ping(ctx)
+	cf()
+	assert.Empty(t, sc.Err())
+	defer client.Close()
+
+	t.Run("EnqueueSmoke", func(t *testing.T) {
+		msg := fmt.Sprintf(`{"num":%d}`, 10)
+		delay := rand.Int()%3000 + 100
+		err := Enqueue(context.Background(), client, queueID, msg, int64(delay))
+		assert.Empty(t, err)
+	})
+
+	t.Run("EnqueueBulk", func(t *testing.T) {
+		for i := 0; i < 10000; i++ {
+			msg := fmt.Sprintf(`{"num":%d}`, i)
+			delay := rand.Int()%3000 + 100
+			err := Enqueue(context.Background(), client, queueID, msg, int64(delay))
+			assert.Empty(t, err)
+		}
+	})
+
+	t.Run("DequeueSmoke", func(t *testing.T) {
+		msgs, err := Dequeue(context.Background(), client, queueID, 10)
+		assert.Empty(t, err)
+		assert.LessOrEqual(t, len(msgs), 10)
+	})
+
+	t.Run("DequeueBulk", func(t *testing.T) {
+		wg := &sync.WaitGroup{}
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for i := 0; i < 100; i++ {
+				msgs, err := Dequeue(context.Background(), client, queueID, 10)
+				assert.Empty(t, err)
+				assert.LessOrEqual(t, len(msgs), 10, "goroutine 1, dequeue bulk")
+				time.Sleep(800 * time.Millisecond)
+			}
+		}()
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for i := 0; i < 100; i++ {
+				msgs, err := Dequeue(context.Background(), client, queueID, 10)
+				assert.Empty(t, err)
+				assert.LessOrEqual(t, len(msgs), 10, "goroutine 2, dequeue bulk")
+				time.Sleep(800 * time.Millisecond)
+			}
+		}()
+		wg.Wait()
+	})
+}
